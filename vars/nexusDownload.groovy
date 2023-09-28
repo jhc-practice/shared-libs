0dadcbba53120bc6cb31ebe3345f0f190d1c13c3
jhc-practice <144198363+jhc-practice@users.noreply.github.com>
2023-09-28 15:14:51 +0530

    Create nexusDownload.groovy

---
+++ vars/nexusDownload.groovy
@@ -0,0 +1,8 @@
+def call(version,outputFile,repoName,groupId,artifactId, extension){
+  def NEXUS_URUL = "http://172.31.23.163:8081"
+  groupId = groupId.replace(".","/")
+  echo groupId
+  sh """
+    curl -u admin:admin -o ${outputFile} -X GET "${NEXUS_URUL}/repository/${repoName}/${groupId}/${artifactId}/${version}/doctor-online-${version}.${extension}"
+  """
+}
